3f3d55972341e32399d5db58c0267a839e0d6b80
lviit <lauriviitala3000@gmail.com>
2019-11-30 18:16:16 +0200

    simple parcel setup with vue

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template >
+    <div class="container">
+        {{ bundler }}
+    </div>
+</template>
+
+<script>
+import Vue from "vue";
+
+export default Vue.extend({
+  data() {
+    return {
+      bundler: "Parcel"
+    };
+  }
+});
+</script>
+
+<style scoped>
+.container {
+  color: red;
+}
+</style>
