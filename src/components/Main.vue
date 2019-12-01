4b9fddfa5b97c4da951fc2fe2b0278a905de1d42
lviit <lauriviitala3000@gmail.com>
2019-12-01 14:48:18 +0200

    add controls for filtering and sort

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
+    <select v-model="section">
+      <template v-for="option in sectionOptions">
+        <option :key="option">{{ option }}</option>
+      </template>
+    </select>
+    <select v-model="sort">
+      <template v-for="option in sortOptions">
+        <option :key="option">{{ option }}</option>
+      </template>
+    </select>
+    <select v-model="window">
+      <template v-for="option in windowOptions">
+        <option :key="option">{{ option }}</option>
+      </template>
+    </select>
+    <input type="checkbox" v-model="viral" />
     <template v-for="image in state.images">
       <Item :key="image.id" :item="image" />
     </template>
@@ -15,9 +31,44 @@ export default {
   mounted() {
     this.actions.getImages();
   },
-
+  data() {
+    const { section, sort, window, viral } = this.state;
+    return {
+      sectionOptions: ["hot", "top", "user"],
+      sortOptions: ["viral", "top", "time", "rising"],
+      windowOptions: ["day", "week", "month", "year", "all"],
+      section,
+      sort,
+      window,
+      viral
+    };
+  },
+  watch: {
+    section(section) {
+      this.actions.setSection(section);
+    },
+    sort(sort) {
+      this.actions.setSort(sort);
+    },
+    window(window) {
+      this.actions.setWindow(window);
+    },
+    viral() {
+      this.actions.toggleViral();
+    },
+    state(state, oldState) {
+      if (
+        state.section !== oldState.section ||
+        state.sort !== oldState.sort ||
+        state.window !== oldState.window ||
+        state.viral !== oldState.viral
+      ) {
+        this.actions.getImages();
+      }
+    }
+  },
   components: {
-    Item,
+    Item
   }
 };
 </script>
